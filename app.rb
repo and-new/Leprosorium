cff0820bc41c72859a17c096584b7e089dc1c614
ol <and-new@rambler.ru>
2017-04-21 14:29:07 +0600

    Save comment to database

--- app.rb
+++ app.rb
@@ -85,8 +85,15 @@ end
 post '/details/:post_id' do
 #получаем переменную из url'a	
 	post_id = params[:post_id]
+
 # получаем переменную из рost-запроса
   	content = params[:content]
 
-	erb "You typed comment #{content} for post #{post_id}"
+# сохранение данных в БД
+  	@db.execute 'insert into Comments 
+  			(content,  created_date, post_id) 
+  			values (?, datetime(), ?)', [content, post_id]
+
+# перенаправление на страницу поста
+ 	redirect to ('/details/' + post_id)
 end
